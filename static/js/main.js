da767703e591b890223cbdd9000f9394c6fc8088
dmymrin1995 <dmymrin1995@gmail.com>
2023-03-31 15:44:55 +0800

    flex

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,17 @@
-$('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
   
